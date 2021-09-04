3cb422379cc425deaa60cdc51da6ba4e7fbef8f7
Gearz <shanegeary1994@gmail.com>
2021-09-04 12:13:33 -0600

    Set up create, update, destroy and strong params in VenuesController

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -10,6 +10,42 @@ module API
                 venue = Venue.find_by(slug: params[:slug])
                 render :json VenueSerializer.new(venue).serialized_json
             end
+
+            def create 
+                venue = Venue.new(venue_params)
+
+                if venue.save
+                    render :json VenueSerializer.new(venue).serialized_json
+                else 
+                    render :json {error: venue.error.message}, status: 422
+                end
+            end
+
+            def update 
+                venue = Venue.find_by(slug: params[:slug])
+
+                if venue.update(venue_params)
+                    render :json VenueSerializer.new(venue).serialized_json
+                else 
+                    render :json {error: venue.error.message}, status: 422
+                end
+            end
+
+            def destroy  
+                venue = Venue.find_by(slug: params[:slug])
+
+                if venue.destroy 
+                    head: no_content
+                else 
+                    render :json {error: venue.error.message}, status: 422
+                end
+            end
+
+            Private
+
+            def venue_params
+                params.require(:venue).permit(:name, :image_url)
+            end
         end
     end
 end
